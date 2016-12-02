8bd06f440343a13b346679b97f3b05205464296d
Justin's Rail <hurulez@gmail.com>
2016-12-02 10:38:41 +0000

    Added list and changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   resources :articles
 
   get 'about', to: 'pages#about'
-  root 'pages#home'
+  root 'articles#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
