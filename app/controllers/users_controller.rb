d20e688715aacff44f0fad0a68a29602d9d92ff0
Justin's Rail <hurulez@gmail.com>
2016-12-04 01:58:08 +0000

    Edit and partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,20 @@ class UsersController < ApplicationController
        @user = User.find(params[:id])
     end
     
+    def edit
+        @user = User.find(params[:id])
+    end
+    
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            flash[:success] = "successfully updated user"
+            redirect_to user_path(@user)
+        else
+            render 'edit'
+        end
+    end
+    
     private 
     def user_params
         params.require(:user).permit(:username, :email, :password)
