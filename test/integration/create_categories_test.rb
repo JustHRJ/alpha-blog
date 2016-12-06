9e4f798f6ec8e65910aa4f940e404bbf558627ef
Justin's Rail <hurulez@gmail.com>
2016-12-06 02:41:55 +0000

    Add category navigation

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -2,7 +2,13 @@ require 'test_helper'
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
    
+   def setup
+     @user = User.create(username: "something", email: 'something@gmail.com', password: 'password', admin: true)
+   end
+   
+   
    test "Get new Category Form and create Category" do
+       sign_in_as(@user, 'password')
        get new_category_path
        assert_template 'categories/new'
        assert_difference 'Category.count', 1 do
@@ -13,6 +19,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
    end
    
    test "Get new category form and create invalid category" do
+          sign_in_as(@user, 'password')
         get new_category_path
        assert_template 'categories/new'
        assert_no_difference 'Category.count' do
@@ -22,5 +29,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
        assert_select 'h2.panel-title'
        assert_select 'div.panel-body'
    end
+   
+   
     
 end
