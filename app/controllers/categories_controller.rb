27e38c7d41d12c1c83a5e99f74734aad1efdc922
Justin's Rail <hurulez@gmail.com>
2016-12-06 05:48:49 +0000

    Edit categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,16 +1,31 @@
 class CategoriesController < ApplicationController
-   before_action :require_admin, only: [:new, :create]
+   before_action :set_category, only: [:show, :edit, :update]
+   before_action :require_admin, only: [:new, :create, :edit, :update]
    
    def require_admin
       if !logged_in? || !current_user.admin?
-         flash[:danger] = "You are not authorized to create"
+         flash[:danger] = "You are not authorized to modify or create content"
          redirect_to categories_path
       end
    end
    
    def show
-      @category = Category.find(params[:id])
+
       @category_articles = @category.articles.paginate(page: params[:page], per_page: 3)
+   end
+   
+   def edit
+
+   end
+   
+   def update
+
+      if @category.update(category_params)
+         flash[:success] = "Successfully edited"
+         redirect_to category_path(@category)
+      else
+         render 'edit'
+      end
    end
    
    def index
@@ -35,6 +50,10 @@ class CategoriesController < ApplicationController
    private 
    def category_params
       params.require(:category).permit(:name)
+   end
+   
+   def set_category
+      @category = Category.find(params[:id])
    end
    
 
