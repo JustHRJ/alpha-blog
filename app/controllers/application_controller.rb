dfa3e6d635da2a24d1aecd82f500c50a4803a6d3
Justin's Rail <hurulez@gmail.com>
2016-12-21 05:26:24 +0000

    last commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
+#by default, these methods are only accessible within the controller and not open to the view. However, by using 'helper_method', it opens up to the view
+#normally, application_helper are where methods are open for the view. 
+#These helper methods are placed here due to the logic of authentication features grouping
   helper_method :current_user, :logged_in?
   
   def current_user
